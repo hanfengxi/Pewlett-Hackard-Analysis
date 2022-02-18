a41ffb864bb27ed949552fb7d00fc1edd31c5528
Fengxi Susan Han <91924218+hanfengxi@users.noreply.github.com>
2022-02-18 18:58:45 -0500

    upload module challenge

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,52 @@
+-- create a Retirement Titles table for employees who are born between January 1, 1952 and December 31, 1955.
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t 
+ON e.emp_no = t.emp_no
+WHERE e.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+ORDER BY e.emp_no;
+
+
+-- use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no, to_date DESC;
+
+
+-- retrieve the number of employees by their most recent job title who are about to retire
+SELECT COUNT(title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+
+
+-- create a Mentorship Eligibility table for current employees who were born between January 1, 1965 and December 31, 1965. 
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibilty
+FROM employees AS e
+INNER JOIN dept_emp AS de 
+ON e.emp_no = de.emp_no
+INNER JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE (de.to_date = '9999-01-01') 
+	AND (e.birth_date BETWEEN '1965-01-01'AND '1965-12-31')
+ORDER BY e.emp_no;
